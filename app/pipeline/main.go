3990ffffa2e4923a77a40c94cecbd98aca5e66a6
AnaisUrlichs <urlichsanais@gmail.com>
2024-01-15 19:12:37 +0000

    minor change
    Signed-off-by: AnaisUrlichs <urlichsanais@gmail.com>

--- app/pipeline/main.go
+++ app/pipeline/main.go
@@ -35,7 +35,7 @@ func build(ctx context.Context) error {
 
 	// define the application build command
 	path := "build/"
-	golang = golang.WithExec([]string{"go", "build", "-o", path})
+	golang = golang.WithExec([]string{"env", "GOOS=darwin", "GOARCH=arm64", "go", "build", "-o", path})
 
 	// get reference to build output directory in container
 	output := golang.Directory(path)
